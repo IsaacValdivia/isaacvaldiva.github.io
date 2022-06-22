876f25db3903ac7bcf1dbe152065ae3200848b37
Isaac Valdivia <isaac.valdivia.dnc@gmail.com>
2022-06-22 12:56:23 +0200

    Updates

---
+++ precache-manifest.dda0b0f03f382f78bf7aa09ff7919482.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbff14e165284d2ea81eb44b208aae8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaa3457333a351bfc741",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "842486566928cf0e7f80",
+    "url": "/static/css/main.d9f2e2d4.chunk.css"
+  },
+  {
+    "revision": "aaa3457333a351bfc741",
+    "url": "/static/js/2.3585bb9e.chunk.js"
+  },
+  {
+    "revision": "a682b37f9a139b2ce8edc8bd4ebe7ab7",
+    "url": "/static/js/2.3585bb9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842486566928cf0e7f80",
+    "url": "/static/js/main.5a1e49ca.chunk.js"
+  },
+  {
+    "revision": "47dff7f7d2288501507d",
+    "url": "/static/js/runtime-main.f5105409.js"
+  },
+  {
+    "revision": "57c13fd4fc2e1a3a54e95eb857551052",
+    "url": "/static/media/headshot4.57c13fd4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ee56e530375b92594c16e22d9217c52.js"
+  "/precache-manifest.dda0b0f03f382f78bf7aa09ff7919482.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
